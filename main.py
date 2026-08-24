425537d84d99f93c0e74fb3a01c2d4f776c6e89f
SGtrinkets <gomezstanley754@gmail.com>
2026-08-24 00:34:00 -0400

    fix: CORS ahora prohibe acceso a API a todo menos el sitio desplegado y localhost

--- main.py
+++ main.py
@@ -23,7 +23,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+        allow_origins=[
+        "https://fastapi-dxui.onrender.com",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
